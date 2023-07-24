ea2faf3bc1f7bef0757d49f07f28c349005e66d5
ReotekDeveloper <developer@reo-tek.com>
2023-07-24 16:50:31 +0300

    ImageTracking software complete

---
+++ Assets/Scripts/ImageTargetManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Vuforia;
+using UnityEngine.Events;
+public class ImageTargetManager : MonoBehaviour
+{
+    public List<GameObject> imageTargets;
+
+    private void Start()
+    {
+        foreach (var target in imageTargets)
+        {
+            var observerEventHandler = target.GetComponent<DefaultObserverEventHandler>();
+            if (observerEventHandler != null)
+            {
+                observerEventHandler.OnTargetFound.AddListener(() => OnImageTargetFound(target));
+                observerEventHandler.OnTargetLost.AddListener(() => OnImageTargetLost(target));
+            }
+            target.transform.GetChild(0).gameObject.SetActive(false);
+        }
+
+
+    }
+
+    private void OnImageTargetFound(GameObject targetObject)
+    {
+        targetObject.transform.GetChild(0).gameObject.SetActive(true);
+
+        foreach (var target in imageTargets)
+        {
+            if (target != targetObject)
+            {
+                target.transform.GetChild(0).gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnImageTargetLost(GameObject targetObject)
+    {
+        targetObject.transform.GetChild(0).gameObject.SetActive(false);
+    }
+}
